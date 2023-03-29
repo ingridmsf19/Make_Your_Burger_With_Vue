082a356dba08faea6be9f281f0a09619acc044fa
Ingrid Mendonca <ingridmsf19@gmail.com>
2023-03-29 14:51:17 -0300

    Implementando area de dashboard na pag de pedidos

--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="main-container">
-    <h1>PEDIDOS</h1>
+    <h1>Gerenciar Pedidos:</h1>
+    <DashboardVue />
   </div>
 </template>
 
 <script>
+import DashboardVue from '../components/Dashboard.vue'
+
 export default {
-  name: 'Pedidos'
+  name: 'Pedidos',
+  components: {
+    DashboardVue
+  }
 }
 </script>
+
+<style scoped>
+
+</style>
+
+
